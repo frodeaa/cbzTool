db1131546f929158b566a0b795b31e32bd57f524
Frode <frode.aa@gmail.com>
2011-04-20 22:18:30 +0200

    Avoid unzipping the CBZ file before creating PDF. (small performance inprovement)
    Simplify the parameters for the app.
    
    java -jar cbrtool.jar -pdf comic.cbz comic.pdf

--- src/main/java/cbztool/CbzToPdf.java
+++ src/main/java/cbztool/CbzToPdf.java
@@ -2,6 +2,7 @@ package cbztool;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -40,162 +41,140 @@ import com.lowagie.text.pdf.PdfWriter;
  * 
  * </p>
  */
-public class CbzToPdf extends SwingWorker<String, String> implements
-		Task<String, Document, String> {
+public class CbzToPdf extends SwingWorker<String, byte[]> implements
+	Task<byte[], Document, String> {
 
-	private final String cbzFile;
-	private final String pdfFile;
-	private final Rectangle pageSize;
-	private UnZip unzipper;
-	private ZipInputStream zipContext;
+    private final String cbzFile;
+    private final String pdfFile;
+    private final Rectangle pageSize;
+    private ZipInputStream zipContext;
 
-	/**
-	 * Create a pdf from a comic cbz file, a zip file containing only images.
-	 * 
-	 * @param cbzFile
-	 *            the comic file path to create pdf from.
-	 * @param pdfFile
-	 *            the path to the PDF that will be created.
-	 */
-	public CbzToPdf(String cbzFile, String pdfFile) {
-		this(cbzFile, pdfFile, 637.28F, 835.7F);
+    /**
+     * Create a pdf from a comic cbz file, a zip file containing only images.
+     * 
+     * @param cbzFile
+     *            the comic file path to create pdf from.
+     * @param pdfFile
+     *            the path to the PDF that will be created.
+     */
+    public CbzToPdf(String cbzFile, String pdfFile) {
+	this(cbzFile, pdfFile, 637.28F, 835.7F);
+    }
+
+    /**
+     * Create a pdf from a comic cbz file, a zip file containing only images.
+     * 
+     * @param cbzFile
+     *            the comic file path to create pdf from.
+     * @param pdfFile
+     *            the path to the PDF that will be created.
+     * @param w
+     *            PDF page width
+     * @param h
+     *            PDF page height
+     */
+    public CbzToPdf(String cbzFile, String pdfFile, float w, float h) {
+	this.cbzFile = cbzFile;
+	this.pdfFile = pdfFile;
+	this.pageSize = new Rectangle(w, h);
+    }
+
+    /**
+     * Create PDF from CBZ file.
+     */
+    @Override
+    protected String doInBackground() throws Exception {
+
+	Document document = prepareContext();
+	Iterator<byte[]> images = iterate(document);
+	while (images.hasNext() && !isCancelled()) {
+	    process(images.next(), document);
+	    setProgress(getProgress() + 1);
 	}
 
-	/**
-	 * Create a pdf from a comic cbz file, a zip file containing only images.
-	 * 
-	 * @param cbzFile
-	 *            the comic file path to create pdf from.
-	 * @param pdfFile
-	 *            the path to the PDF that will be created.
-	 * @param w
-	 *            PDF page width
-	 * @param h
-	 *            PDF page height
-	 */
-	public CbzToPdf(String cbzFile, String pdfFile, float w, float h) {
-		this.cbzFile = cbzFile;
-		this.pdfFile = pdfFile;
-		this.pageSize = new Rectangle(w, h);
+	endContext(document);
+	Logger.getLogger(getClass().getName())
+		.finest("Created pdf: " + pdfFile);
+	return pdfFile;
+    }
+
+    @Override
+    public Document prepareContext() throws InitializationException {
+	try {
+	    zipContext = new ZipInputStream(new FileInputStream(cbzFile));
+	} catch (FileNotFoundException e1) {
+	    throw new InitializationException("Unable to open zip archive: "
+		    + cbzFile, e1);
 	}
-
-	/**
-	 * Create PDF from CBZ file.
-	 */
-	@Override
-	protected String doInBackground() throws Exception {
-
-		Document document = prepareContext();
-		Iterator<String> images = iterate(document);
-		while (images.hasNext() && !isCancelled()) {
-			process(images.next(), document);
-			setProgress(getProgress() + 1);
-		}
-
-		endContext(document);
-		Logger.getLogger(getClass().getName()).info("Created pdf: " + pdfFile);
-		return pdfFile;
+	Document document = new Document(pageSize);
+	try {
+	    PdfWriter.getInstance(document, new BufferedOutputStream(
+		    new FileOutputStream(pdfFile)));
+	} catch (FileNotFoundException e) {
+	    throw new InitializationException("Unable to create output: "
+		    + pdfFile, e);
+	} catch (DocumentException e) {
+	    throw new InitializationException("Unable to initialize PdfWriter",
+		    e);
 	}
+	document.setMarginMirroring(true);
+	document.setMargins(0, 0, 0, 0);
 
-	/**
-	 * Initialize the task.
-	 */
-	public Document prepareContext() throws InitializationException {
+	document.open();
+	return document;
+    }
 
-		File source = new File(cbzFile);
-		File file = new File(System.getProperty("java.io.tmpdir"), source
-				.getName());
-		file.deleteOnExit();
+    /**
+     * Add image to PDF document.
+     * 
+     * @param value
+     *            the image path.
+     * @param context
+     *            the PDF document.
+     */
+    @Override
+    public String process(byte[] value, Document context)
+	    throws ProcessException {
 
-		unzipper = new UnZip(cbzFile, UnZip.ALL(file.getAbsolutePath()));
-		zipContext = unzipper.prepareContext();
-		Document document = new Document(pageSize);
+	try {
+	    com.lowagie.text.Image pdfImage = com.lowagie.text.Image
+		    .getInstance(value);
+	    pdfImage.setAlignment(Element.ALIGN_CENTER);
 
-		try {
-			PdfWriter.getInstance(document, new BufferedOutputStream(
-					new FileOutputStream(pdfFile)));
-		} catch (FileNotFoundException e) {
-			throw new InitializationException("Unable to create output: "
-					+ pdfFile, e);
-		} catch (DocumentException e) {
-			throw new InitializationException("Unable to initialize PdfWriter",
-					e);
-		}
-		document.setMarginMirroring(true);
-		document.setMargins(0, 0, 0, 0);
+	    if (pdfImage.getWidth() > pdfImage.getHeight()) {
+		pdfImage.setRotationDegrees(90);
+		pdfImage.rotate();
+	    }
 
-		document.open();
-		return document;
+	    pdfImage.scaleToFit(637.28f, 835.7f);
+	    context.add(pdfImage);
+	    context.newPage();
+	} catch (BadElementException e) {
+	    throw new ProcessException("Unable to add image to pdf", e);
+	} catch (IOException e) {
+	    throw new ProcessException("Unable to add image to pdf", e);
+	} catch (DocumentException e) {
+	    throw new ProcessException("Unable to add image to pdf", e);
 	}
+	return null;
+    }
 
-	/**
-	 * Add image to PDF document.
-	 * 
-	 * @param value
-	 *            the image path.
-	 * @param context
-	 *            the PDF document.
-	 */
-	public String process(String value, Document context)
-			throws ProcessException {
-		try {
-			Logger.getLogger(getClass().getName()).finer(
-					String.format("Adding %s to %s", value, pdfFile));
-			Image image = Image.getInstance(value);
-			image.setAlignment(Element.ALIGN_CENTER);
-
-			if (image.getWidth() > image.getHeight()) {
-				image.setRotationDegrees(90);
-				image.rotate();
-			}
-
-			image.scaleToFit(637.28f, 835.7f);
-			context.add(image);
-			context.newPage();
-
-			new File(value).delete();
-		} catch (BadElementException e) {
-			throw new ProcessException("BadElement", e);
-		} catch (DocumentException e) {
-			throw new ProcessException("DocumentException", e);
-		} catch (IOException e) {
-			throw new ProcessException("IOException", e);
-		}
-		return null;
+    /**
+     * Clean up the task, close all open stream.
+     */
+    public void endContext(Document context) {
+	context.close();
+	try {
+	    zipContext.close();
+	} catch (IOException e) {
 	}
+    }
 
-	/**
-	 * Clean up the task, close all open stream.
-	 */
-	public void endContext(Document context) {
-		context.close();
-		unzipper.endContext(zipContext);
-	}
-
-	/**
-	 * Iterator for retriving the paths to the images that should be processed.
-	 */
-	public Iterator<String> iterate(Document context) {
-		final Iterator<ZipEntry> itr = unzipper.iterate(zipContext);
-		return new Iterator<String>() {
-
-			public boolean hasNext() {
-				return itr.hasNext();
-			}
-
-			public String next() {
-				try {
-					return unzipper.process(itr.next(), zipContext);
-				} catch (ProcessException e) {
-					e.printStackTrace();
-					return null;
-				}
-			}
-
-			public void remove() {
-			}
-
-		};
-	}
+    /**
+     * Iterator for retrieve the image bytes in the ZIP.
+     */
+    public Iterator<byte[]> iterate(Document context) {
+	return ZipIterator.bytes(zipContext);
+    }
 
 }
--- src/main/java/cbztool/MainApp.java
+++ src/main/java/cbztool/MainApp.java
@@ -12,113 +12,120 @@ import cbztool.share.Delegator;
  */
 public final class MainApp {
 
-	/**
-	 * Print usage details for this application.
-	 * 
-	 * @param delegators
-	 *            the delegators registered that usage details will be created
-	 *            from.
-	 */
-	private static void printUsage(List<Delegator> delegators) {
-		System.out.println("Usage: -g <options> <parameters>");
-		System.out.println("options:");
-		for (Delegator delegator : delegators) {
-			System.out.println(delegator.getDescription());
-		}
+    /**
+     * Print usage details for this application.
+     * 
+     * @param delegators
+     *            the delegators registered that usage details will be created
+     *            from.
+     */
+    private static void printUsage(List<Delegator> delegators) {
+	System.out.println("Usage: java -jar cbztool.jar [PARAMETERS]");
+	System.out.println("Manipulate CBZ archives.");
+	System.out.println("Example: java -jar cbztool.jar -pdf "
+		+ "comic.cbz comic.pdf\n");
+	System.out.println(" Parameters:");
+	for (Delegator delegator : delegators) {
+	    System.out.println("  " + delegator.getDescription());
+	}
+    }
+
+    /**
+     * A Delegator that converts a CBZ file into a PDF file.
+     */
+    private static class DelegateCbzToPdf implements Delegator {
+	private static final int OPT = 0;
+	private static final int PDF_FILE = 2;
+	private static final int CBZ_FILE = 1;
+
+	@Override
+	public boolean accept(String[] args) {
+	    return args.length == 3 && args[OPT].equals("-pdf");
 	}
 
-	/**
-	 * A Delegator that converts a CBZ file into a PDF file.
-	 */
-	private static class DelegateCbzToPdf implements Delegator {
-
-		@Override
-		public boolean accept(String[] args) {
-			return args.length == 4 && args[0].equals("-g")
-					&& args[1].equals("cbzToPdf");
-		}
-
-		@Override
-		public String getDescription() {
-			return "cbzToPdf <cbzFile> <pdfFile>";
-		}
-
-		@Override
-		public void handle(String[] args) throws Exception {
-			File inputCbz = new File(args[2]);
-
-			if (!inputCbz.isFile() || !inputCbz.exists()) {
-				printError("CBZ file not found:" + args[2], this);
-			}
-
-			File outputPdf = new File(args[3]);
-			if (!inputCbz.isFile()) {
-				printError("Not a valid pdf output file:" + args[3], this);
-			}
-
-			final CbzToPdf cbzPdf = new CbzToPdf(inputCbz.getAbsolutePath(),
-					outputPdf.getAbsolutePath());
-					
-			// execute this on current thread.
-			cbzPdf.doInBackground();					
-		}
-
+	@Override
+	public String getDescription() {
+	    return "-pdf <cbzFile> <pdfFile>  - convert CBZ to a PDF";
 	}
 
-	/** Delegators that handles the inputs for this application. */
-	private static final List<Delegator> DELEGATORS;
+	@Override
+	public void handle(String[] args) throws Exception {
+	    File inputCbz = new File(args[CBZ_FILE]);
 
-	/** Initialize delegators. */
-	static {
-		DELEGATORS = new ArrayList<Delegator>();
-		DELEGATORS.add(new DelegateCbzToPdf());
+	    if (!inputCbz.isFile() || !inputCbz.exists()) {
+		printError("CBZ file not found:" + args[CBZ_FILE], this);
+	    }
+
+	    File outputPdf = new File(args[PDF_FILE]);
+	    if (!inputCbz.isFile()) {
+		printError("Not a valid pdf output file:" + args[PDF_FILE],
+			this);
+	    }
+
+	    final CbzToPdf cbzPdf = new CbzToPdf(inputCbz.getAbsolutePath(),
+		    outputPdf.getAbsolutePath());
+
+	    // execute this on current thread.
+	    cbzPdf.doInBackground();
 	}
 
-	/**
-	 * Run applications
-	 * 
-	 * @param args
-	 *            the parameters to pass, will print usage if no arguments or no
-	 *            valid arguments are provided.
-	 * @throws InterruptedException
-	 *             if executing task is canceled.
-	 * @throws ExecutionException
-	 *             if any error happends while executing the task.
-	 */
-	public static void main(String[] args) throws InterruptedException,
-			ExecutionException {
+    }
 
-		for (Delegator delegator : DELEGATORS) {
-			if (delegator.accept(args)) {
-				try {
-					delegator.handle(args);
-				} catch (Exception e) {
-					printError(e.getMessage(), null);
-				}
-				System.exit(0);
-			}
+    /** Delegators that handles the inputs for this application. */
+    private static final List<Delegator> DELEGATORS;
+
+    /** Initialize delegators. */
+    static {
+	DELEGATORS = new ArrayList<Delegator>();
+	DELEGATORS.add(new DelegateCbzToPdf());
+    }
+
+    /**
+     * Run applications
+     * 
+     * @param args
+     *            the parameters to pass, will print usage if no arguments or no
+     *            valid arguments are provided.
+     * @throws InterruptedException
+     *             if executing task is canceled.
+     * @throws ExecutionException
+     *             if any error happends while executing the task.
+     */
+    public static void main(String[] args) throws InterruptedException,
+	    ExecutionException {
+
+	for (Delegator delegator : DELEGATORS) {
+	    if (delegator.accept(args)) {
+		try {
+		    delegator.handle(args);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    printError(e.getMessage(), null);
 		}
-		printUsage(DELEGATORS);
+		System.exit(0);
+	    }
+	}
+	printUsage(DELEGATORS);
+    }
+
+    /**
+     * Print error message and correct usage example.
+     * 
+     * @param errorMessage
+     *            the error message to print.
+     * @param delegator
+     *            the delegator that usage example us provided from, if
+     *            <code>null</code> no help message/usage message will be
+     *            printed.
+     */
+    private static void printError(String errorMessage, Delegator delegator) {
+	System.err.println(errorMessage);
+
+	if (delegator != null) {
+	    System.out.println("Usage : -g " + delegator.getDescription());
 	}
 
-	/**
-	 * Print error message and correct usage example.
-	 * 
-	 * @param errorMessage
-	 *            the error message to print.
-	 * @param delegator
-	 *            the delegator that usage example us provided from, if
-	 *            <code>null</code> no help message/usage message will be
-	 *            printed.
-	 */
-	private static void printError(String errorMessage, Delegator delegator) {
-		System.err.println(errorMessage);
-
-		if (delegator != null) {
-			System.out.println("Usage : -g " + delegator.getDescription());
-		}
-
-		System.exit(-1);
-	}
+	System.exit(-1);
+    }
 
 }
